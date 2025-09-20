31247ed8f68d317038ecfceb4dbcecf4a213e10a
Marco Willy <44058572+marcowilly@users.noreply.github.com>
2025-09-19 23:16:45 -0300

    Frist commit

---
+++ src/main/java/com/idiesoftware/baterias24hrs/Baterias24hrsApplication.java
@@ -0,0 +1,13 @@
+package com.idiesoftware.baterias24hrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baterias24hrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Baterias24hrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idiesoftware/baterias24hrs/Baterias24hrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idiesoftware.baterias24hrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baterias24hrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
